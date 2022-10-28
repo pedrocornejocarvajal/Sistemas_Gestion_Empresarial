f78ae8860c523e381fdbc289f6a20e42c74b5d7d
pedrocornejocarvajal <75272975+pedrocornejocarvajal@users.noreply.github.com>
2022-10-28 13:38:23 +0200

    28/10/2022

---
+++ 08-EjerciciosU8/08-EjerciciosU8_ASP_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _08_EjerciciosU8_ASP_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
